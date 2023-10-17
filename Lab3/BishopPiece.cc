9bd1ed06c419262921b108c49d37c510e2e85b8e
jung1230 <alanchen20072@gmail.com>
2023-10-17 11:28:59 -0400

    found a bug in chessboard.cc

--- Lab3/BishopPiece.cc
+++ Lab3/BishopPiece.cc
@@ -1,4 +1,5 @@
 #include "BishopPiece.hh"
+#include <iostream>
 
 using Student::BishopPiece;
 
@@ -11,40 +12,63 @@ Type BishopPiece::getType(){
 }
 
 bool BishopPiece::canMoveToLocation(int row, int col){ // Haven't done yet
+
+
+    std::cout << "12312231" << std::endl;
+
     int move_row = row - getRow();
     int move_col = col - getColumn();
-    if((move_col+move_row != 0) || (move_col-move_row != 0) || (move_row-move_col != 0)) // Not moving like a bishop
-        return false;
+    if((move_col + move_row != 0) && (move_col - move_row != 0) && (move_row - move_col != 0)){
+        std::cout << "32" << std::endl;
+        return false;        
+    } 
+
     // Have to check whether there is piece in the moving place
     if((move_row > 0) && (move_col > 0)){
         for(int i = 1; i < move_row; i++){
             for(int j = 1; j < move_col; j++){
-                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr)
-                    return false;
+                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr){
+                    std::cout << "33" << std::endl;
+
+                    return false;                    
+                }
+
             }
         }
     }
     else if((move_row > 0) && (move_col < 0)){
         for(int i = 1; i < move_row; i++){
             for(int j = -1; j > move_col; j--){
-                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr)
-                    return false;
+                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr){
+                    std::cout << "34" << std::endl;
+
+                    return false;                    
+                }
+
             }
         }
     }
     else if((move_row < 0) && (move_col > 0)){
         for(int i = -1; i > move_row; i--){
             for(int j = 1; j < move_col; j++){
-                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr)
-                    return false;
+                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr){
+                    std::cout << "35" << std::endl;
+
+                    return false;                    
+                }
+
             }
         }
     }
     else{
         for(int i = -1; i > move_row; i--){
             for(int j = -1; j > move_col; j--){
-                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr)
-                    return false;
+                if(_board.getPiece(getRow() + i, getColumn() + j) != nullptr){
+                    std::cout << "36" << std::endl;
+
+                    return false;                    
+                }
+
             }
         }
     }
